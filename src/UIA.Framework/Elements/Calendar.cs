c90728d099793c4595203bb12302171bbccef8c0
Dzmitry Simakou <dzmitry_simakou@epam.com>
2018-12-17 11:13:35 +0300

    hotfix for Calendar.class

--- src/UIA.Framework/Elements/Calendar.cs
+++ src/UIA.Framework/Elements/Calendar.cs
@@ -58,24 +58,24 @@
         public string Value => ((ValuePattern)this.GetCurrentPattern(ValuePattern.Pattern)).Current.Value;
 
         /// <inheritdoc/>
-        public AutomationElement GetItem(int row, int column) => ActionHandler.Perform(() => ((GridPattern)RawElement.this.GetCurrentPattern(GridPattern.Pattern)).GetItem(row, column));
+        public AutomationElement GetItem(int row, int column) => ActionHandler.Perform(() => ((GridPattern)this.GetCurrentPattern(GridPattern.Pattern)).GetItem(row, column));
 
         /// <inheritdoc/>
-        public void Scroll(ScrollAmount horizontalAmount = 0, ScrollAmount verticalAmount = 0) => ActionHandler.Perform(() => ((ScrollPattern)RawElement.this.GetCurrentPattern(ScrollPattern.Pattern)).Scroll(horizontalAmount, verticalAmount));
+        public void Scroll(ScrollAmount horizontalAmount = 0, ScrollAmount verticalAmount = 0) => ActionHandler.Perform(() => ((ScrollPattern)this.GetCurrentPattern(ScrollPattern.Pattern)).Scroll(horizontalAmount, verticalAmount));
 
         /// <inheritdoc/>
-        public void SetScrollPercent(double horizontalPercent, double verticalPercent) => ActionHandler.Perform(() => ((ScrollPattern)RawElement.this.GetCurrentPattern(ScrollPattern.Pattern)).SetScrollPercent(horizontalPercent, verticalPercent));
+        public void SetScrollPercent(double horizontalPercent, double verticalPercent) => ActionHandler.Perform(() => ((ScrollPattern)this.GetCurrentPattern(ScrollPattern.Pattern)).SetScrollPercent(horizontalPercent, verticalPercent));
 
         /// <inheritdoc/>
-        public AutomationElement[] GetSelection() => ActionHandler.Perform(() => ((SelectionPattern)RawElement.this.GetCurrentPattern(SelectionPattern.Pattern)).Current.GetSelection());
+        public AutomationElement[] GetSelection() => ActionHandler.Perform(() => ((SelectionPattern)this.GetCurrentPattern(SelectionPattern.Pattern)).Current.GetSelection());
 
         /// <inheritdoc/>
-        public AutomationElement[] GetColumnHeaders() => ActionHandler.Perform(() => ((TablePattern)RawElement.this.GetCurrentPattern(TablePattern.Pattern)).Current.GetColumnHeaders());
+        public AutomationElement[] GetColumnHeaders() => ActionHandler.Perform(() => ((TablePattern)this.GetCurrentPattern(TablePattern.Pattern)).Current.GetColumnHeaders());
 
         /// <inheritdoc/>
-        public AutomationElement[] GetRowHeaders() => ActionHandler.Perform(() => ((TablePattern)RawElement.this.GetCurrentPattern(TablePattern.Pattern)).Current.GetRowHeaders());
+        public AutomationElement[] GetRowHeaders() => ActionHandler.Perform(() => ((TablePattern)this.GetCurrentPattern(TablePattern.Pattern)).Current.GetRowHeaders());
 
         /// <inheritdoc/>
-        public void SetValue(string value) => ActionHandler.Perform(() => ((ValuePattern)RawElement.this.GetCurrentPattern(ValuePattern.Pattern)).SetValue(value));
+        public void SetValue(string value) => ActionHandler.Perform(() => ((ValuePattern)this.GetCurrentPattern(ValuePattern.Pattern)).SetValue(value));
     }
 }
